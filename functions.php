3eacacad0e8a3cb1c60ed28acaebcf6a2c4880cf
Pedro de Carvalho <p@43.lc>
2015-07-21 08:59:02 +0100

    fixing warning about enqueue scripts

--- functions.php
+++ functions.php
@@ -3,14 +3,12 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-		
-}
-wp_enqueue_script( 'glossary', get_stylesheet_directory_uri() . '/js/glossary.js', array( 'jquery' ), '20150330', true );
+    wp_enqueue_script( 'glossary', get_stylesheet_directory_uri() . '/js/glossary.js', array( 'jquery' ), '20150330', true );
 wp_localize_script( 'glossary', 'MyAjax', 
   	array( 'ajaxurl' => admin_url( 'admin-ajax.php' ),
     	   'gettermsNonce' => wp_create_nonce( 'myajax-term-nonce' ),
 	 ) );
-	 
+}
 
 add_action('wp_ajax_get_terms','prefix_ajax_get_terms');
 add_action('wp_ajax_nopriv_get_terms','prefix_ajax_get_terms');
@@ -19,9 +17,7 @@ add_action('wp_ajax_nopriv_get_terms','prefix_ajax_get_terms');
 function prefix_ajax_get_terms(){
 	$nonce = $_POST['getterms_nonce'];
 	
-  	//if ( ! wp_verify_nonce( $nonce, 'myajax-term-nonce' ) ) {
-	 //      die ( 'Not allowed.');
-	//}
+  	//if ( ! wp_verify_nonce( $nonce, 'myajax-term-nonce' ) ) { die ( 'Not allowed.');}
 	// getting the term from ajax post
 	$term_text = sanitize_text_field($_POST['term_text']); 
 	$solution  = "";
